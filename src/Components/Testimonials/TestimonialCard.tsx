f7c4ba706ed4ad2a400e850117ba8378a0bc3fee
shafik720 <shafikrasel5@gmail.com>
2024-07-01 07:23:37 +0600

    created testimonial section

--- src/Components/Testimonials/TestimonialCard.tsx
+++ src/Components/Testimonials/TestimonialCard.tsx
@@ -1,45 +1,69 @@
+"use client";
 import Image from "next/image";
 import testImg from "../../assets/img/street-21.jpg";
-import { Button } from "../ui/button";
 import { useState } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import leftArrow from "@/assets/img/left-arrow-svgrepo-com.png";
+import rightArrow from "@/assets/img/right-arrow-svgrepo-com.png";
+import { getImg, getShortBio, getText } from "@/utils/carouselFunctions";
 
 const TestimonialCard = () => {
-    let[count, setCount] = useState(0) ;
+  let [count, setCount] = useState(0);
 
-    function handleClick(){
-        // console.log(count);
-        if(count < 2){
-            setCount(++count);
-        }else if(count == 2){
-            setCount(0);
-        }        
+  function handleClickIncrease() {
+    if (count < 2) {
+      setCount(++count);
+    } else if (count == 2) {
+      setCount(0);
     }
+  }
+
+  function handleClickDecrease() {
+    if (count == 0) {
+      setCount(2);
+    } else if (count > 0) {
+      setCount(--count);
+    }
+  }
 
   return (
     <div className="bg-[#4A4B7C] w-[80%] relative rounded">
       <div className=" py-14 ps-10 text-gray-200  w-[70%]">
         <hr className="w-20 border-b-4 mb-10 border-slate-400" />
-        <p className="text-xl font-bold transition-all">
-          {count == 0 && `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et dicta
-          consequuntur obcaecati harum voluptate ut omnis ipsum dolores enim!
-          Tempore sint porro vitae, nam, laboriosam`}
-          {count == 1 && `A Professional Photographer from a Small City in Bangladesh. I am a self taught Nerd. I have been doing Photography for more than a Decade.`}
-          {count == 2 && `I have won several awards and four International Awards for Photography. Street & Wild-life Photography is my favourite Genre. I also have a good hand In Wedding & Portfolio Area. Some of my sample works are given below.`}
-        </p>
+        <TransitionGroup className="relative min-h-32">
+          <CSSTransition key={count} timeout={500} classNames="fade">
+            <p className="text-xl  absolute">{getText(count)}</p>
+          </CSSTransition>
+        </TransitionGroup>
 
         <hr className=" border-b mb-10  border-slate-400 mt-10" />
-        <div className="">
-          <p className="text-lg font-semibold">Jonathan Wick</p>
-          <p className="text-gray-400">Local Assasin</p>
+        <TransitionGroup className="relative min-h-12">
+          <CSSTransition key={count} timeout={500} classNames="fade">
+            <div className="absolute">{getShortBio(count)}</div>
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
+
+      <TransitionGroup className="">
+          <CSSTransition key={count} timeout={500} classNames="fade">
+            {getImg(count)}
+          </CSSTransition>
+        </TransitionGroup>
+
+      <div className="flex justify-center items-center gap-5 absolute bottom-[14%] translate-y-[-14%] right-[25%] translate-x-[-25%]  ">
+        <div
+          onClick={handleClickIncrease}
+          className="border-4 p-2 rounded-full cursor-pointer"
+        >
+          <Image src={leftArrow} alt="" width={30} />
+        </div>
+        <div
+          onClick={handleClickDecrease}
+          className="border-4 p-2 rounded-full cursor-pointer"
+        >
+          <Image src={rightArrow} alt="" width={30} />
         </div>
       </div>
-      <Image
-        src={testImg}
-        alt="testimonial pic"
-        width={440}
-        className="absolute -top-20 -right-48 shadow-2xl drop-shadow-2xl"
-      />
-      <Button onClick={handleClick}>Click</Button>
     </div>
   );
 };
---
+++ src/utils/carouselFunctions.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+
+import clientImg1 from "../assets/img/street-21.jpg";
+import clientImg2 from "../assets/img/trevor-buntin-m-Qz-ALy-EB-T4-unsplash-1.jpg";
+import clientImg3 from "../assets/img/woman in black white.jpg";
+
+export const getText = (count: number) => {
+  switch (count) {
+    case 0:
+      return `Working with this photographer was an absolute pleasure. Their professionalism, creativity, and attention to detail resulted in stunning photos that exceeded our expectations. Highly recommend!`;
+    case 1:
+      return `Our family photoshoot was an incredible experience. The photographer made us feel comfortable and captured beautiful moments we will cherish forever. Exceptional work, truly talented, and recommended!`;
+    case 2:
+      return `The wedding photos are beyond beautiful. This photographer captured every special moment perfectly. Their artistic vision and dedication to their craft are evident in every shot. Recommended!`;
+    default:
+      return "";
+  }
+};
+
+export const getShortBio = (count: number) => {
+  switch (count) {
+    case 2:
+      return (
+        <div>
+          <p className="text-lg font-semibold">Jonathan Wick</p>
+          <p className="text-gray-400">Local Assasin</p>
+        </div>
+      );
+    case 1:
+      return (
+        <div>
+          <p className="text-lg font-semibold">Abraham Lincoln</p>
+          <p className="text-gray-400">Vampire Hunter</p>
+        </div>
+      );
+    case 0:
+      return (
+        <div>
+          <p className="text-lg font-semibold">Shafiqul Hasan Rasel</p>
+          <p className="text-gray-400">Web Developer</p>
+        </div>
+      );
+    default:
+      return "";
+  }
+};
+
+export const getImg = (count: number) => {
+  switch (count) {
+    case 0:
+      return (
+        <Image
+          src={clientImg1}
+          alt="testimonial pic"
+          width={440}
+          className="absolute -top-20 -right-48 shadow-2xl drop-shadow-2xl"
+        />
+      );
+    case 1:
+      return (
+        <Image
+          src={clientImg2}
+          alt="testimonial pic"
+          width={440}
+          className="absolute -top-20 -right-48 shadow-2xl drop-shadow-2xl"
+        />
+      );
+    case 2:
+      return (
+        <Image
+          src={clientImg3}
+          alt="testimonial pic"
+          width={440}
+          className="absolute -top-20 -right-48 shadow-2xl drop-shadow-2xl"
+        />
+      );
+    default:
+      return (
+        <Image
+          src={clientImg1}
+          alt="testimonial pic"
+          width={440}
+          className="absolute -top-20 -right-48 shadow-2xl drop-shadow-2xl"
+        />
+      );
+  }
+};
